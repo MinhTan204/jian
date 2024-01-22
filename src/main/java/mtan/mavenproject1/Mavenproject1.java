2926acc60884e64a70a998f4ad7bff1067e965c6
minhtan204 <tandm.lnbp@gmail.com>
2024-01-22 16:17:38 +0700

    first commit

---
+++ src/main/java/mtan/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mtan.mavenproject1;
+
+/**
+ *
+ * @author TAN
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
